767803fc14a3d5882db66a1c2ae5271851df750f
Alexandr Orlov <alexandr.orlov@thomsonreuters.com>
2019-04-26 17:08:49 +0400

    movies

---
+++ precache-manifest.eab47ecf18c5650270ddc8e0af96ba34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bceee99b64978e5c10e",
+    "url": "/static/css/main.33096010.chunk.css"
+  },
+  {
+    "revision": "8bceee99b64978e5c10e",
+    "url": "/static/js/main.8bceee99.chunk.js"
+  },
+  {
+    "revision": "238b267041b77fc95e76",
+    "url": "/static/js/1.238b2670.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/static/media/background.d2edb29f.jpg"
+  },
+  {
+    "revision": "e784a195f2df02c53d5d1e7d96bbb22c",
+    "url": "/index.html"
+  }
+];
